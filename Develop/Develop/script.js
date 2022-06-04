aa764be1fa6ed19dd46c3503baacbd8bcba21f3f
RubinoD1 <Rubinod1@gmail.com>
2022-06-04 03:44:59 -0400

    hour check is working for one timeblock, need to test multiple

--- Develop/Develop/script.js
+++ Develop/Develop/script.js
@@ -1,16 +1,82 @@
 //Links to the "id = currentDay", so the calender can be set to display as its text on screen. 
 var currentDay = $("#currentDay");
+var scheduleHour = $(".time-block");
+
+
 
 //sets the format of the calender to be displayed in the header
 var calenderSet = moment().format('MMMM Do YYYY, h:mm:ss a');
-//format for the moment.js hour to be used in detrmining if the hour is past, present, or future on the schedule
-var trackHour = moment().format("H");
+//format for the moment.js hour to be used in detrmining if the hour is in the past, present, or future on the schedule
 
+var convert = parseInt(moment().format("H"));
+var trackHour = convert
+console.log(trackHour)
+
+var thisBlockHr = parseInt(scheduleHour.attr("data-hour"));
+console.log(thisBlockHr)
+
+
+var test = function(){
+if (thisBlockHr > trackHour){
+    console.log("A")
+    scheduleHour.addClass("future");
+    scheduleHour.removeClass("present past");
+}
+if (thisBlockHr < trackHour){
+    console.log("B")
+    scheduleHour.addClass("past");
+    scheduleHour.removeClass("present future"); 
+}
+if (thisBlockHr == trackHour){
+    console.log("C")
+    scheduleHour.addClass("present");
+    scheduleHour.removeClass("past future");
+}
+};
+
+test();
 //sets the text in the HTML document with the moment.js calender defined in by var calenderSet 
 currentDay.text(calenderSet);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function to detrmine if a timeblock is in the past, present, or future and sets the color accordingly. 
+setScheduleColor = function() {
+    scheduleHour.each(function(){
+         hourTimeBlock =(this);
+         checkHour = parseInt(this.attr(data-index-hour));
+    
+    if (checkHour < trackHour) {
+         
+    }
+    if (checkHour > trackHour) {
+        
+    }
+    if  (checkHour == trackHour) {
+        
+    }
+    
+
+    });
+}
+*/
 
 
 
