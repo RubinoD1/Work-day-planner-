2620e458e91c7999896a0369d2fa9696139cbe88
RubinoD1 <Rubinod1@gmail.com>
2022-06-04 00:41:19 -0400

    set timeblock layout, starting hour js for colors

--- Develop/Develop/script.js
+++ Develop/Develop/script.js
@@ -3,6 +3,26 @@ var currentDay = $("#currentDay");
 
 //sets the format of the calender to be displayed in the header
 var calenderSet = moment().format('MMMM Do YYYY, h:mm:ss a');
+//format for the moment.js hour to be used in detrmining if the hour is past, present, or future on the schedule
+var trackHour = moment().format("H");
 
 //sets the text in the HTML document with the moment.js calender defined in by var calenderSet 
 currentDay.text(calenderSet);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Notes
+1. Time does not update in real time in the header. 
+
+*/
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Work Day Scheduler Starter Code
+
+-Created a JS file, so that the HTML doucment wouldn't get out of hand with all the code being added. 
+
+-Current day is displayed at the top of the calender
+I went with the full date, year, and timer format. When working with a planner you want this information. Even if this is meant to be a work day planner, being able to quickly confirm the date is a helpful feature to have. 
+
