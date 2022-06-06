81406c8362b65cd9b8a1a5dc7800ae9e8566d065
RubinoD1 <Rubinod1@gmail.com>
2022-06-06 02:31:38 -0400

    function for checking hours is properly looping through and setting colors, cleaned up the js file a bit.

--- Develop/Develop/script.js
+++ Develop/Develop/script.js
@@ -2,69 +2,67 @@
 var currentDay = $("#currentDay");
 var scheduleHour = $(".time-block");
 
-
-
-
 //sets the format of the calender to be displayed in the header
 var calenderSet = moment().format('MMMM Do YYYY, h:mm:ss a');
-//format for the moment.js hour to be used in detrmining if the hour is in the past, present, or future on the schedule
-
-var convert = parseInt(moment().format("H"));
-var trackHour = convert
-//console.log(trackHour)
-
-//var thisBlockHr = parseInt(scheduleHour.attr("data-hour"));
-//console.log(thisBlockHr)
-
-
-
-var test = function(){
-    $(scheduleHour).each(function(){
-  
-var thisBlockHr = parseInt(scheduleHour.attr("data-hour"));
-console.log(thisBlockHr)  
-   
-        
-if (thisBlockHr > trackHour){
-    console.log("A")
-    scheduleHour.addClass("future");
-    scheduleHour.removeClass("present past");
-}
-if (thisBlockHr < trackHour){
-    console.log("B")
-    scheduleHour.addClass("past");
-    scheduleHour.removeClass("present future"); 
-}
-if (thisBlockHr == trackHour){
-    console.log("C")
-    scheduleHour.addClass("present");
-    scheduleHour.removeClass("past future");
-}
-});
-}
-//test();
 //sets the text in the HTML document with the moment.js calender defined in by var calenderSet 
 currentDay.text(calenderSet);
 
 
+//format for the moment.js hour to be used in detrmining if the hour is in the past, present, or future on the schedule
+var trackHour = parseInt(moment().format("H"));
+//console.log(trackHour)
 
-///save and load functions 
-//using the jquery .on() function to listen for a click event on the "".saveBtn"
-$(".saveBtn").on("click", function () {
-    //jquery looks for all the ".text-input" classes and gets their value. 
-   var text = $(this).siblings(".text-save").val(); 
-   var time = $(this).parent().attr("id"); 
-   //saves to local storage 
-     localStorage.setItem(time, text);
-})
-
-//load LocalStorage, for the schedule we need the text that was inputted into the textarea of the HTML and the scheduled hour.  
-var loadInfo =function() {
-    $("#8 .text-save").val(localStorage.getItem("8"));
-    $("#15 .text-save").val(localStorage.getItem("15"));
+//function to set the time-block colors.
+function setColor(){
+    scheduleHour.each(function() {
+    var storeBlock = $(this);
+    //convert string to number, so it can be compared to the current hour, that was also made into a number in "var trackHour"
+    var timeBlockHr = parseInt(storeBlock.attr("id"));
+//        
+if (timeBlockHr > trackHour){
+   
+    storeBlock.addClass("future");
+    storeBlock.removeClass("present past");
+}
+if (timeBlockHr < trackHour){
+    
+    storeBlock.addClass("past");
+    storeBlock.removeClass("present future"); 
+}
+if (timeBlockHr == trackHour){
+    
+    storeBlock.addClass("present");
+    storeBlock.removeClass("past future");
+}
+});
 }
 
+
+//save and load functions 
+//using the jquery .on() function to listen for a click event on the "".saveBtn"
+$(".saveBtn").on("click", function () {
+    //jquery looks for all the ".text-save" classes and gets their value. 
+   var textConent = $(this).siblings(".text-save").val(); 
+   var timeblockHr = $(this).parent().attr("id"); 
+   //saves to local storage 
+     localStorage.setItem(timeblockHr, textConent);
+});
+
+//load LocalStorage, for the schedule we need the text that was inputted into the textarea of the HTML and the corresponding time-block.  
+var loadInfo =function() {
+    $("#8 .text-save").val(localStorage.getItem("8"));
+    $("#9 .text-save").val(localStorage.getItem("9"));
+    $("#10 .text-save").val(localStorage.getItem("10"));
+    $("#11 .text-save").val(localStorage.getItem("11"));
+    $("#12 .text-save").val(localStorage.getItem("12"));
+    $("#13 .text-save").val(localStorage.getItem("13"));
+    $("#14 .text-save").val(localStorage.getItem("14"));
+    $("#15 .text-save").val(localStorage.getItem("15"));
+    $("#16 .text-save").val(localStorage.getItem("16"));
+};
+
 loadInfo();
+setColor();
 
 
 
@@ -77,25 +75,9 @@ loadInfo();
 
 
 
-/*function to detrmine if a timeblock is in the past, present, or future and sets the color accordingly. 
-setScheduleColor = function() {
-    scheduleHour.each(function(){
-         hourTimeBlock =(this);
-         checkHour = parseInt(this.attr(data-index-hour));
-    
-*/
 
 
 
 
 
 
-
-
-
-
-
-/*Notes
-1. Time does not update in real time in the header. 
-2. is the moment hour in 24hr format to check p.m.?
-*/
