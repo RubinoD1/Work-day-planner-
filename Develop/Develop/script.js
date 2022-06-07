72bc148bbdb676e37d04eeeb471c11bbba5a9cba
RubinoD1 <Rubinod1@gmail.com>
2022-06-07 18:29:15 -0400

    adjusted HTML, added more comments to JS file, strated README

--- Develop/Develop/script.js
+++ Develop/Develop/script.js
@@ -1,46 +1,50 @@
-//Links to the "id = currentDay", so the calender can be set to display as its text on screen. 
+//Links to the "id = currentDay" in the HTML, so the calender can be set to display as its text on screen. 
 var currentDay = $("#currentDay");
+//Links to the "time-block" class 
 var scheduleHour = $(".time-block");
 
-//sets the format of the calender to be displayed in the header
+//sets the format of the calender to be displayed in the header using moment.js
 var calenderSet = moment().format('MMMM Do YYYY, h:mm:ss a');
-//sets the text in the HTML document with the moment.js calender defined in by var calenderSet 
+//sets the text in the HTML document with the moment.js calender defined by var calenderSet 
 currentDay.text(calenderSet);
 
 
 //format for the moment.js hour to be used in detrmining if the hour is in the past, present, or future on the schedule
+//using parse to convert the string into a number
 var trackHour = parseInt(moment().format("H"));
-//console.log(trackHour)
+
 
 //function to set the time-block colors.
 function setColor(){
     scheduleHour.each(function() {
     var storeBlock = $(this);
-    //convert string to number, so it can be compared to the current hour, that was also made into a number in "var trackHour"
+    //convert string to number, so it can be compared to the current hour.
     var timeBlockHr = parseInt(storeBlock.attr("id"));
-        
-if (timeBlockHr > trackHour){
-//sets CSS style depending on the time-blocks relation to the current hour.    
+//sets CSS style depending on the time-blocks relation to the current hour.        
+if (timeBlockHr > trackHour)
+{
     storeBlock.addClass("future");
     storeBlock.removeClass("present past");
-}
-if (timeBlockHr < trackHour){
+
+} else (timeBlockHr < trackHour)
+{
     storeBlock.addClass("past");
     storeBlock.removeClass("present future"); 
-}
-if (timeBlockHr == trackHour){
+
+} (timeBlockHr == trackHour)
+{
     storeBlock.addClass("present");
     storeBlock.removeClass("past future");
 }
 });
 }
 
-
 //save and load functions 
 //using the jquery .on() function to listen for a click event on the "".saveBtn"
 $(".saveBtn").on("click", function () {
     //jquery looks for all the ".text-save" classes and gets their value. 
    var textConent = $(this).siblings(".text-save").val(); 
+   //jquery searches for the "id" where the time-block's value has been set.  
    var timeblockHr = $(this).parent().attr("id"); 
    //saves to local storage 
      localStorage.setItem(timeblockHr, textConent);
--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Work Day Scheduler Starter Code
 
--Created a JS file, so that the HTML doucment wouldn't get out of hand with all the code being added. 
+#General Notes
+- Created a JS file, so that the HTML doucment wouldn't get out of hand with all the code being added.
+- I didn't like the Font Awesome save icons. So, I decided to use a button labeled "Save" instead of replacing the default button. 
+- I used Bootstap for the adjustable break points that media query would normally do in the CSS, "row, col-sm , col-md, col-lg", etc.  
+
 
 -Current day is displayed at the top of the calender
-I went with the full date, year, and timer format. When working with a planner you want this information. Even if this is meant to be a work day planner, being able to quickly confirm the date is a helpful feature to have. 
-
+I went with the full date, year, and time format. When working with a planner you to be able to quickly confirm all of this information.  
