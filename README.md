eb2d7d9f0b107e260d74a19120e38c4ec9616def
RubinoD1 <Rubinod1@gmail.com>
2023-04-14 08:17:03 -0400

    finished changes to README and cleaned up comments in JS file

--- README.md
+++ README.md
@@ -7,24 +7,21 @@ GitHub live page:https://rubinod1.github.io/Work-day-planner-/
 
 # Introduction
 
-Works Day Planner is a JavaScript built day planner that allows a user to plan their work day by assigning tasks to timeblocks throughout the day. 
+Works Day Planner is a JavaScript-built day planner that allows a user to plan their work day by assigning tasks to time blocks throughout the day. 
 
 # Features
 
 - Dynamically updating date and time in the site's header. 
-- Timeblocks are color coded based on the time of day: Green = future, Red = Present, Grey = past. 
+- Time blocks are color coded based on the time of day: green = future, red = present, and grey = past. 
 - Uses local storage to save user entries. 
 
 
 # Technoligies 
 - HTML / CSS
 - JavaScript 
+- jQuery
 - Bootstrap 
-- Moment.JS
-
-# Liscence
-
-MIT 
+- Moment.js
 
 # Screenshots
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,32 +7,27 @@ function clockUpdate() {
     `<h2>${moment().format('MMMM Do YYYY, h:mm:ss a')} </h2>`;
 }
 
-//format for the moment.js hour to be used in detrmining if the hour is in the past, present, or future on the schedule.
+//format for the moment.js hour to be used in determining if the hour is in the past, present, or future on the schedule.
 const trackHour = parseInt(moment().format("H")); //using parse to convert the string into a number.
 
 
 //function to set the time-block colors.
-function setColor(){
+function setColor() {
   scheduleHour.each(function() {
     var storeBlock = $(this);
     //convert string to number, so it can be compared to the current hour.
     var timeBlockHr = parseInt(storeBlock.attr("id"));
-//sets CSS style depending on the time-blocks relation to the current hour.        
-if (timeBlockHr > trackHour)
-{
+   //sets CSS style depending on the time-blocks relation to the current hour.        
+   if (timeBlockHr > trackHour) {
     storeBlock.addClass("future");
     storeBlock.removeClass("present past");
-
-}else if (timeBlockHr < trackHour)
-{
+   } else if (timeBlockHr < trackHour) {
     storeBlock.addClass("past");
     storeBlock.removeClass("present future"); 
-
-}else if (timeBlockHr == trackHour)
-{
+   } else if (timeBlockHr == trackHour) {
     storeBlock.addClass("present");
     storeBlock.removeClass("past future");
-}
+   }
 });
 }
 
@@ -40,12 +35,12 @@ if (timeBlockHr > trackHour)
 //save and load functions 
 //using the jquery .on() function to listen for a click event on the "".saveBtn"
 $(".saveBtn").on("click", function () {
-    //jquery looks for all the ".text-save" classes and gets their value. 
+   //jquery looks for all the ".text-save" classes and gets their value. 
    var textConent = $(this).siblings(".text-save").val(); 
    //jquery searches for the "id" where the time-block's value has been set.  
    var timeblockHr = $(this).parent().attr("id"); 
    //saves to local storage 
-     localStorage.setItem(timeblockHr, textConent);
+   localStorage.setItem(timeblockHr, textConent);
 });
 
 //load localStorage 
@@ -81,9 +76,6 @@ loadInfo(); // local storage
 setColor(); //updates the time block css based on time of day 
 clockUpdate(); //sets the date/time in the header based on the current time.  
 setInterval(clockUpdate, 1000); //timer to update the time in the HTML in real time. 1000 milliseconds = 1 second. 
-
-
-
 
 
 
